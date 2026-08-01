9e6643b7d167a5eab0a80a693a7f055a6a40455a
Ashwin Kumar C <ashwinkumartth@gmail.com>
2026-08-01 12:13:54 +0530

    es lint fix

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'coverage']), // remove coverage to fix es lint
+  globalIgnores(['dist']), // remove coverage to fix es lint
   {
     files: ['**/*.{js,jsx}'],
     extends: [
--- jest.config.cjs
+++ jest.config.cjs
@@ -9,4 +9,12 @@ module.exports = {
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
+
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: '.',
+      outputName: 'junit.xml',
+    }],
+  ],
 }
